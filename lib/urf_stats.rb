d932ac66e82a87b0c902a6b31259c2160352573c
Roy Liu <carsomyr@gmail.com>
2015-04-07 23:22:52 -0400

    Add multithreading to the `SaveMatchesWorker` for faster match JSON retrieval

--- lib/urf_stats.rb
+++ lib/urf_stats.rb
@@ -21,15 +21,16 @@ require "riot"
 module UrfStats
   CONTEST_START_TIME = DateTime.strptime("2015-03-31T22:00:00-07:00")
 
-  REQUEST_SPACING_MILLIS = 10
+  REQUEST_SPACING_MILLIS = 40
 
   JOB_LENGTH_MILLIS = 600000
+
+  CONCURRENCY = 12
 
   def self.save_matches
     job_start_time = DateTime.now
     job_start_time_millis = job_start_time.strftime("%Q").to_i
     bucket_end_time = (job_start_time - 1.hour).to_time.to_i
-    n_max_requests = JOB_LENGTH_MILLIS / REQUEST_SPACING_MILLIS
 
     request_param_tuples = ChallengeBucketCounter.all.reduce([]) do |acc, cbc|
       acc + (cbc.bucket_time.to_time.to_i...bucket_end_time).step(5.minutes).map do |time|
@@ -37,7 +38,7 @@ module UrfStats
       end
     end.sort do |lhs, rhs|
       lhs[0] <=> rhs[0]
-    end[0, n_max_requests]
+    end
 
     # Read randomized 5-minute match id buckets from the challenge API.
     request_param_tuples.each do |time, cbc|
@@ -70,26 +71,35 @@ module UrfStats
       sleep(REQUEST_SPACING_MILLIS / 1000.0)
     end
 
+    thread_queue = Queue.new
+    CONCURRENCY.times { thread_queue.push(true) }
+
     # Now fill in any missing match JSON; process in small batches to prevent unbounded heap growth.
-    Riot::Api::Match.where(content: nil).find_in_batches(batch_size: 10).each do |riot_api_matches|
-      riot_api_matches.each do |riot_api_match|
-        job_current_time_millis = DateTime.now.strftime("%Q").to_i
+    Riot::Api::Match.where(content: nil).find_in_batches(batch_size: 3).each do |riot_api_matches|
+      job_current_time_millis = DateTime.now.strftime("%Q").to_i
 
-        return \
-          if job_current_time_millis - job_start_time_millis > JOB_LENGTH_MILLIS
+      return \
+        if job_current_time_millis - job_start_time_millis > JOB_LENGTH_MILLIS
 
-        response = Riot::Api.client(riot_api_match.region).match(riot_api_match.match_id)
+      thread_queue.shift
 
-        case response.status
-          when 200
-            riot_api_match.content = response.body
-            riot_api_match.save!
-          else
-            # If 429 (rate limit exceeded) or anything unexpected, return immediately.
-            return
+      Thread.new do
+        riot_api_matches.each do |riot_api_match|
+          response = Riot::Api.client(riot_api_match.region).match(riot_api_match.match_id)
+
+          case response.status
+            when 200
+              riot_api_match.content = response.body
+              riot_api_match.save!
+            else
+              # If 429 (rate limit exceeded) or anything unexpected, return immediately.
+              return
+          end
+
+          sleep(REQUEST_SPACING_MILLIS / 1000.0)
         end
 
-        sleep(REQUEST_SPACING_MILLIS / 1000.0)
+        thread_queue.push(true)
       end
     end
   end
