446b04da3053d024676086eed2bbbb3019a9bad3
Roy Liu <carsomyr@gmail.com>
2015-04-17 00:34:47 -0400

    Add the `sessions` table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 18) do
+ActiveRecord::Schema.define(version: 19) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,16 @@ ActiveRecord::Schema.define(version: 18) do
   add_index "riot_api_static_entities", ["entity_id", "type"], name: "index_riot_api_static_entities_on_entity_id_and_type", unique: true, using: :btree
   add_index "riot_api_static_entities", ["entity_id"], name: "index_riot_api_static_entities_on_entity_id", using: :btree
   add_index "riot_api_static_entities", ["type"], name: "index_riot_api_static_entities_on_type", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "stats", force: :cascade do |t|
     t.string   "region",                  limit: 4,             null: false
